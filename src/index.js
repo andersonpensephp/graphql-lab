e652e4692708812cee99ac21ad9aafce82e4e3b1
Anderson <andersonpensephp@hotmail.com>
2023-01-19 18:16:13 -0300

    query

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log('Hello!!!');
+import { ApolloServer, gql } from 'apollo-server';
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => 'Hello'
+  }
+}
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen(4000).then(({ url }) => console.log(`Server listening on url ${url}`));
